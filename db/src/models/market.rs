1f4e804d72d32829f09f75402ab96c0dc8e7e135
jassbawa <jasspreetsbawa@gmail.com>
2025-11-30 17:25:12 +0530

    db: added public key in db

--- db/src/models/market.rs
+++ db/src/models/market.rs
@@ -20,6 +20,12 @@ pub struct MarketRow {
     pub end_timestamp: i64,
     pub created_slot: i64,
 
+    pub collateral_mint: String,
+    pub collateral_vault: String,
+    pub yes_mint: String,
+    pub no_mint: String,
+    pub bump: i16,
+
     pub resolved: bool,
     pub resolved_outcome: Option<String>,
 
@@ -45,6 +51,12 @@ pub struct Market {
     pub end_timestamp: i64,
     pub created_slot: i64,
 
+    pub collateral_mint: String,
+    pub collateral_vault: String,
+    pub yes_mint: String,
+    pub no_mint: String,
+    pub bump: i16,
+
     pub resolved: bool,
     pub resolved_outcome: Option<String>,
 
@@ -60,15 +72,26 @@ impl From<MarketRow> for Market {
             market_address: row.market_address,
             creator_address: row.creator_address,
             program_id: row.program_id,
+
             title: row.title,
             description: row.description,
             category: row.category,
+
             yes_option: row.yes_option,
             no_option: row.no_option,
+
             end_timestamp: row.end_timestamp,
             created_slot: row.created_slot,
+
+            collateral_mint: row.collateral_mint,
+            collateral_vault: row.collateral_vault,
+            yes_mint: row.yes_mint,
+            no_mint: row.no_mint,
+            bump: row.bump,
+
             resolved: row.resolved,
             resolved_outcome: row.resolved_outcome,
+
             created_at: row.created_at,
             updated_at: row.updated_at,
         }
--- db/src/queries/market.rs
+++ db/src/queries/market.rs
@@ -16,6 +16,11 @@ pub async fn create_market(
     no_option: &str,
     end_timestamp: i64,
     created_slot: i64,
+    collateral_mint: &str,
+    collateral_vault: &str,
+    yes_mint: &str,
+    no_mint: &str,
+    bump: i16,
 ) -> Result<i64> {
     let row = sqlx::query(
         r#"
@@ -30,9 +35,14 @@ pub async fn create_market(
             yes_option,
             no_option,
             end_timestamp,
-            created_slot
+            created_slot,
+            collateral_mint,
+            collateral_vault,
+            yes_mint,
+            no_mint,
+            bump
         )
-        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10, $11)
+        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16)
         RETURNING id
         "#,
     )
@@ -47,6 +57,11 @@ pub async fn create_market(
     .bind(no_option)
     .bind(end_timestamp)
     .bind(created_slot)
+    .bind(collateral_mint)
+    .bind(collateral_vault)
+    .bind(yes_mint)
+    .bind(no_mint)
+    .bind(bump)
     .fetch_one(pool)
     .await?;
 
@@ -57,22 +72,27 @@ pub async fn get_market_by_address(pool: &PgPool, address: &str) -> Result<Optio
     let row = sqlx::query_as::<_, MarketRow>(
         r#"
         SELECT
-            id,
-            market_id,
-            market_address,
-            creator_address,
-            program_id,
-            title,
-            description,
-            category,
-            yes_option,
-            no_option,
-            end_timestamp,
-            created_slot,
-            resolved,
-            resolved_outcome,
-            created_at,
-            updated_at
+        id,
+           market_id,
+           market_address,
+           creator_address,
+           program_id,
+           title,
+           description,
+           category,
+           yes_option,
+           no_option,
+           end_timestamp,
+           created_slot,
+           collateral_mint,
+           collateral_vault,
+           yes_mint,
+           no_mint,
+           bump,
+           resolved,
+           resolved_outcome,
+           created_at,
+           updated_at
         FROM markets
         WHERE market_address = $1
         LIMIT 1
